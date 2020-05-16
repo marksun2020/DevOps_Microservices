a35b6d3413409f39f670f6d2b7703f5f3a8b1203
Alexander Weimann <alexander.weimann@outlook.com>
2020-05-16 19:56:43 +0200

    Tasks 1 - 7

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,18 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath='marksun2020/api'
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run --image=$dockerpath api --port=80
 
+sleep 1m
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods --all-namespaces
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward api 8000:80
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,11 +5,13 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath='marksun2020/api'
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login docker.io -u marksun2020 -p XXXX
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
